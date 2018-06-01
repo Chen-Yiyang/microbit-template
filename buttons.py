5dedd79661bf15093b3174eb7565a0877a285155
WHK <weehekun@gmail.com>
2018-06-02 05:56:10 +0800

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,12 @@
+from microbit import *
+
+while True:
+    if # What is the condition the micro:bit is looking out for?
+        display.show(Image.HAPPY)
+        
+    elif # What other conditions could the micro:bit be looking out for?
+        display.show(Image.SNAKE)
+        break
+        
+    else:
+        display.show(Image.SAD)
