6dce3fd82589b1e7a615b7f8a9d83074f2e06e24
WHK <weehekun@gmail.com>
2018-06-02 06:00:09 +0800

    Create boat_image.py

---
+++ boat_image.py
@@ -0,0 +1,14 @@
+from microbit import *
+
+# 2 + 2 == 4
+# "2" + "2" == "22", though.
+# "string 1" + "string 2" == "string 1string 2"
+# notice that there's no spacing between ^ "string 1" and "string 2"
+
+boat =       "05050:"+\
+             "05050:"+\
+             "05050:"+\
+             "99999:"+\
+             "09990"
+
+display.show(boat)
