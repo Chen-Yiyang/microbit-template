c9d91b99cf052493cbfc8ccfbb496a9bb4ab72e6
WHK <weehekun@gmail.com>
2018-06-02 06:48:29 +0800

    Update fireflies.py

--- fireflies.py
+++ fireflies.py
@@ -2,21 +2,29 @@ from microbit import *
 import random
 import radio
 
-flash = []
+flash = [] # a list. do you remember the boats?
 for x in range(9, -1, -1):
-flash = [Image().invert()*(x/9) for x in range(9, -1 , -1)]
+    empty_img = Image()
+    fully_lit_img = 
+    multiplied_brightness = 
+    
+    # add it into flash
+    flash.append(multiplied_brightness)
 
 radio.on()
 
 while True:
-    if button_a.was_pressed():
+    if button_a.was_pressed(): # IMPORTANT: we use was_pressed() here to make sure we don't spam the radio.
         radio.send('flash')
     
-    incoming = radio.receive()
+    # receive the message
+    incoming = 
     if incoming == 'flash':
-        sleep(random.randint(50, 350))
+        procrastination = random.randint(50, 350) # I'll do it - in a minute.
+        # when you procrastinate, what do you do?
         display.show(flash, delay=100, wait=False)
         
-        if random.randint(0,5) == 2:
-            sleep(500)
-            radio.send('flash')
+        # roll a dice and if you land on 2, send another signal!
+        if # generate a random number and check its value.
+            sleep(500) # wait a little while before sending the signal
+            # send the signal!
